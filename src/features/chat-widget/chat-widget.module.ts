0151d61dd2686c310d668a9ce36dcc1e5e0acc1f
dheerajt112 <dheerajt.maisondauraine.com>
2025-11-14 13:35:05 +0530

    fixes

--- src/features/chat-widget/chat-widget.module.ts
+++ src/features/chat-widget/chat-widget.module.ts
@@ -3,11 +3,12 @@ import { ChatWidgetController } from './chat-widget.controller';
 import { ChatWidgetService } from './chat-widget.service';
 import { ChatWidgetGateway } from './chat-widget.gateway';
 import { PrismaService } from '../../prisma/prisma.service';
-import { KafkaProducerService } from '../kafka/kafka-producer.service';
+import { KafkaModule } from '../kafka/kafka.module';
 
 @Module({
+  imports: [KafkaModule],
   controllers: [ChatWidgetController],
-  providers: [ChatWidgetService, ChatWidgetGateway, PrismaService, KafkaProducerService],
+  providers: [ChatWidgetService, ChatWidgetGateway, PrismaService],
   exports: [ChatWidgetService, ChatWidgetGateway],
 })
 export class ChatWidgetModule {}
